37591df3d5bf0a89d247228ab2a95639b3dae7e8
ssysm <syua9708@flhs.us>
2018-02-10 23:32:45 -0500

    add docs template

---
+++ docs/src/highlight.js
@@ -0,0 +1,23 @@
+import visit from 'unist-util-visit';
+
+import hljs from 'highlight.js/lib/highlight';
+import javascript from 'highlight.js/lib/languages/javascript';
+import json from 'highlight.js/lib/languages/json';
+
+hljs.registerLanguage('javascript', javascript);
+hljs.registerLanguage('json', json);
+
+/**
+ * Adapted from remark-highlight.js
+ * https://github.com/ben-eb/remark-highlight.js
+ */
+export default function attacher() {
+    function visitor(node) {
+        if (node.lang) {
+            node.type = 'html';
+            node.value = `<pre class='hljs'>${hljs.highlightAuto(node.value, [node.lang]).value}</pre>`;
+        }
+    }
+
+    return ast => visit(ast, 'code', visitor);
+}
